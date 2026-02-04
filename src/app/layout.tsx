b7707025d292711d39e1474ff626090c441368ee
Kaycee Garcia <kayceegarcia2004@gmail.com>
2026-02-04 15:02:20 -0700

    Add ClickSpark animation to entire page with custom green color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DockNavigation from "@/components/DockNavigation";
+import ClickSpark from "@/components/ClickSpark";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,10 +25,22 @@ export default function RootLayout({
         className={`${inter.variable} antialiased flex flex-col min-h-screen m-0 p-0`}
         style={{ fontFamily: "var(--font-inter), sans-serif" }}
       >
-        <DockNavigation />
-        <main className="flex-1">
-          {children}
-        </main>
+        <ClickSpark
+          sparkColor="#07450C"
+          sparkSize={15}
+          sparkRadius={80}
+          sparkCount={12}
+          duration={500}
+          easing="ease-out"
+          extraScale={1.2}
+        >
+          <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', width: '100%' }}>
+            <DockNavigation />
+            <main className="flex-1">
+              {children}
+            </main>
+          </div>
+        </ClickSpark>
       </body>
     </html>
   );
