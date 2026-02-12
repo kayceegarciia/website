3d12ad6dcd4b0bbcc7a253db1f2d0bad58414212
Kaycee Garcia <kayceegarcia2004@gmail.com>
2026-02-12 12:04:02 -0700

    changed background speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
             hueShift={0}
             noiseIntensity={0}
             scanlineIntensity={0}
-            speed={0.5}
+            speed={1.2}
             scanlineFrequency={0}
             warpAmount={0}
           />
